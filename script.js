2de1e353e5a13afc5ce7655fbff84f330a19d761
Athul64 <athulsylesh23@gmail.com>
2024-07-27 21:23:16 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+function navigateToLogin() {
+    window.location.href = 'pages/login.html';
+}
+
+function navigateToIndex() {
+    window.location.href = '../index.html';
+}
+
+function navigateToSignup() {
+    window.location.href = 'pages/signup.html';
+}
+
+function navigateToStart() {
+    window.location.href = 'pages/login.html';
+}
+
+//logout nav
+function navigateToLogout() {
+    window.location.href = '../index.html';
+}
+
+//1st quiz
+function navigateToCarquiz(){
+    window.location.href= 'f1.html';
+}
+
+//2nd quiz
+function navigateTohisquiz(){
+    window.location.href= 'indianhistory.html'
+}
+
+//3rd quiz
+function navigateTogkquiz(){
+    window.location.href= 'gk.html'
+}
+
+//4th quiz
+function navigateTosportsquiz(){
+    window.location.href= 'sports.html'
+}
+
+//5th quiz
+function navigateTotechquiz(){
+    window.location.href= 'technology.html'
+}
+
+//6th quiz
+function navigateTogeoquiz(){
+    window.location.href= 'geo.html'
+}
